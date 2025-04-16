359ffc0e11a7d8da16b61868b934d34cbf492f2a
Daniel <deco2449584@gmail.com>
2025-04-15 23:45:54 -0500

    fix3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/weding/", // Debe coincidir con el nombre de tu repositorio
+  base: "./", // Cambiar a ruta relativa
 });
