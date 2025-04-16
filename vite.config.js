9017ec70fb75c5ec5094a2ed6a57024494b736a3
Daniel <deco2449584@gmail.com>
2025-04-16 00:04:40 -0500

    fix big 2

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,23 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".")[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          } else if (/mp3|wav/i.test(extType)) {
+            extType = "audio";
+          } else if (/mp4|webm/i.test(extType)) {
+            extType = "video";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
   },
+  publicDir: "public", // Asegura que los archivos de public se copien a dist
   server: {
     port: 3000, // Puerto en el que se ejecuta el servidor
     open: true, // Abre el navegador automáticamente al iniciar el servidor
