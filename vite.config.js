07bb78153bee215404c2a52be94a7cceb2a144e8
Daniel <deco2449584@gmail.com>
2025-04-15 23:50:51 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Cambiar a ruta relativa
+  base: "/",
+  server: {
+    port: 3000, // Puerto en el que se ejecuta el servidor
+    open: true, // Abre el navegador automáticamente al iniciar el servidor
+    host: true, // Escucha en todas las interfaces (0.0.0.0), permitiendo acceso desde otros dispositivos
+  },
 });
